b656a460a5652b87670b8de56cd07b50bbf392a4
Zarpperia <castiellgames@gmail.com>
2024-03-04 21:40:09 -0300

    final da aula

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // CLasse abstrata não deixa eu fazer um objeto a partir dela... o comando abaixo dá erro...
+        //Veiculo meuVeiculo = new Veiculo();
+
     }
 }
